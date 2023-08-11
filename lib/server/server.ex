e8168589993893f8f066f4e731445f53b26853e3
Erikson Kaszubowski <erikson23@gmail.com>
2023-08-11 15:04:47 -0300

    Implement aan sync todo server referred by PID

---
+++ lib/server/server.ex
@@ -0,0 +1,63 @@
+defmodule TodoList.Server do
+  @moduledoc """
+  Creates an async Todo list manager.
+  """
+
+  def start(entries \\ []) do
+    pid = spawn(fn -> listen(TodoList.new(entries)) end)
+    Process.register(pid, :todo)
+  end
+
+  defp listen(todo_list) do
+    new_todo =
+      receive do
+        {:add, entry} ->
+          TodoList.add_entry(todo_list, entry)
+
+        {:remove, id} ->
+          TodoList.delete_entry(todo_list, id)
+
+        {:update, id, field, new_data} ->
+          TodoList.update_entry(todo_list, {id, field}, new_data)
+
+        {:retrieve, caller, date} ->
+          send(caller, {:result, TodoList.entries(todo_list, date)})
+          todo_list
+
+        {:print} ->
+          IO.puts(todo_list)
+          todo_list
+
+        invalid_msg ->
+          {:error, "Invalid msg: #{invalid_msg}"}
+      end
+
+    listen(new_todo)
+  end
+
+  def add_entry(server, entry) do
+    send(server, {:add, entry})
+  end
+
+  def remove_entry(server, id) do
+    send(server, {:remove, id})
+  end
+
+  def update_entry(server, id, field, new_data) do
+    send(server, {:update, id, field, new_data})
+  end
+
+  def retrieve(server, date) do
+    send(server, {:retrieve, self(), date})
+
+    receive do
+      {:result, result} -> result
+    after
+      2000 -> {:error, :timeout}
+    end
+  end
+
+  def print(server) do
+    send(server, {:print})
+  end
+end
