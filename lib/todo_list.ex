076e3335242aaa872d8f457186c8ca3b39958857
Erikson Kaszubowski <erikson23@gmail.com>
2023-08-11 14:02:56 -0300

    Implement Collectable protocol.

--- lib/todo_list.ex
+++ lib/todo_list.ex
@@ -99,6 +99,19 @@ defimpl String.Chars, for: TodoList do
   end
 end
 
+defimpl Collectable, for: TodoList do
+  def into(todo_list) do
+    {todo_list, &into_callback/2}
+  end
+
+  defp into_callback(todo_list, {:cont, entry}) do
+    TodoList.add_entry(todo_list, entry)
+  end
+
+  defp into_callback(todo_list, :done), do: todo_list
+  defp into_callback(_todo_list, :halt), do: :ok
+end
+
 defmodule TodoList.CsvImporter do
   @moduledoc """
   Import todo list from CSV file with `Date | Title` format.
