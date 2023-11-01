91e0bab7eb9b2fc45f2dd3e70f86bb19fa9d5e85
Erikson Kaszubowski <erikson.k@ufsc.br>
2023-11-01 16:00:36 -0300

    Implement timeout for Todo Server.

--- lib/todo/server.ex
+++ lib/todo/server.ex
@@ -2,47 +2,49 @@ defmodule Todo.Server do
   @moduledoc """
   Creates an async Todo list manager.
   """
+
+  @expiry_idle_timeout :timer.seconds(10)
+
   use GenServer, restart: :temporary
 
   @impl GenServer
   def init(name) do
-    send(self(), {:real_init, name})
-    # {:ok, {name, Todo.Database.get(name) || Todo.List.new()}}
-    {:ok, nil}
+    {:ok, {name, Todo.Database.get(name) || Todo.List.new()}, @expiry_idle_timeout}
   end
 
   @impl GenServer
   def handle_cast({:add, entry}, {name, list}) do
     new_list = Todo.List.add_entry(list, entry)
     Todo.Database.store(name, new_list)
-    {:noreply, {name, new_list}}
+    {:noreply, {name, new_list}, @expiry_idle_timeout}
   end
 
   def handle_cast({:remove, id}, {name, list}) do
     new_list = Todo.List.delete_entry(list, id)
     Todo.Database.store(name, new_list)
-    {:noreply, {name, new_list}}
+    {:noreply, {name, new_list}, @expiry_idle_timeout}
   end
 
   def handle_cast({:update, id, field, new_data}, {name, list}) do
     new_list = Todo.List.update_entry(list, {id, field}, new_data)
     Todo.Database.store(name, new_list)
-    {:noreply, {name, new_list}}
+    {:noreply, {name, new_list}, @expiry_idle_timeout}
   end
 
   def handle_cast({:print}, {name, list}) do
     IO.puts(list)
-    {:noreply, {name, list}}
+    {:noreply, {name, list}, @expiry_idle_timeout}
   end
 
   @impl GenServer
   def handle_call({:retrieve, date}, _from, {name, list}) do
-    {:reply, Todo.List.entries(list, date), {name, list}}
+    {:reply, Todo.List.entries(list, date), {name, list}, @expiry_idle_timeout}
   end
 
   @impl GenServer
-  def handle_info({:real_init, name}, _state) do
-    {:noreply, {name, Todo.Database.get(name) || Todo.List.new()}}
+  def handle_info(:timeout, {name, list}) do
+    IO.puts("Stopping ToDo Server for #{name}")
+    {:stop, :normal, {name, list}}
   end
 
   def start_link(name) do
