d4482ea4ba94947343fd4359427cedc357b662d9
Erikson Kaszubowski <erikson.k@ufsc.br>
2023-11-13 14:20:50 -0300

    Update DBWorker function to work with poolboy.

--- lib/todo/database.ex
+++ lib/todo/database.ex
@@ -8,18 +8,6 @@ defmodule Todo.Database do
   @pool_size 3
   @db_folder "./persist"
 
-  #  def start_link do
-  #    File.mkdir_p!(@db_folder)
-  #
-  #    children = Enum.map(1..@pool_size, &worker_spec/1)
-  #    Supervisor.start_link(children, strategy: :one_for_one)
-  #  end
-  #
-  #  defp worker_spec(worker_id) do
-  #    default_spec = {Todo.DatabaseWorker, {@db_folder, worker_id}}
-  #    Supervisor.child_spec(default_spec, id: worker_id)
-  #  end
-  #
   def child_spec(_) do
     IO.puts("Starting ToDo.DB...")
     File.mkdir_p!(@db_folder)
@@ -36,10 +24,6 @@ defmodule Todo.Database do
       __MODULE__,
       fn worker_id -> DatabaseWorker.store(worker_id, key, data) end
     )
-
-    #    key
-    #    |> get_worker()
-    #    |> DatabaseWorker.store(key, data)
   end
 
   def get(key) do
@@ -47,13 +31,5 @@ defmodule Todo.Database do
       __MODULE__,
       fn worker_id -> DatabaseWorker.get(worker_id, key) end
     )
-
-    #    key
-    #    |> get_worker()
-    #    |> DatabaseWorker.get(key)
   end
-
-  #  defp get_worker(key) do
-  #    :erlang.phash2(key, @pool_size) + 1
-  #  end
 end
