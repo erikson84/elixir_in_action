715f3c35f60418cf0a602a86f235d53c3b1e0cee
Erikson Kaszubowski <erikson.k@ufsc.br>
2023-11-13 13:31:58 -0300

    Correct Cache tests.

--- test/cache_test.exs
+++ test/cache_test.exs
@@ -2,17 +2,17 @@ defmodule Todo.CacheTest do
   use ExUnit.Case
 
   test "Verify servers pids" do
-    {:ok, cache} = Todo.Cache.start()
-    bob_pid = Todo.Cache.server_process(cache, "Bob's list")
+    # {:ok, cache} = Todo.Cache.start()
+    bob_pid = Todo.Cache.server_process("Bob's list")
 
-    assert bob_pid != Todo.Cache.server_process(cache, "Alice's list")
-    assert bob_pid == Todo.Cache.server_process(cache, "Bob's list")
+    assert bob_pid != Todo.Cache.server_process("Alice's list")
+    assert bob_pid == Todo.Cache.server_process("Bob's list")
   end
 
   test "Verify Todo operations with server" do
-    {:ok, cache} = Todo.Cache.start()
+    # {:ok, cache} = Todo.Cache.start()
 
-    alice = Todo.Cache.server_process(cache, "alice")
+    alice = Todo.Cache.server_process("alice")
     entry = %{date: ~D[2022-01-01], title: "Shave my armpits."}
     Todo.Server.add_entry(alice, entry)
     entries = Todo.Server.retrieve(alice, ~D[2022-01-01])
